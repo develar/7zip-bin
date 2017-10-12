553900826d98a289e0b7e315a3a8e737a3999f3e
develar <develar@gmail.com>
2017-10-12 08:31:34 +0200

    feat: SZA_ARCHIVE_TYPE

--- 7x.sh
+++ 7x.sh
@@ -3,6 +3,6 @@
 sz_program=${SZA_PATH:-7za}
 
 case $1 in
-  -d) "$sz_program" e -si -so -txz ;;
-   *) "$sz_program" a f -si -so -txz -mx${SZA_COMPRESSION_LEVEL:-9} ;;
+  -d) "$sz_program" e -si -so -t${SZA_ARCHIVE_TYPE:xz} ;;
+   *) "$sz_program" a f -si -so -t${SZA_ARCHIVE_TYPE:xz} -mx${SZA_COMPRESSION_LEVEL:-9} ;;
 esac 2> /dev/null
